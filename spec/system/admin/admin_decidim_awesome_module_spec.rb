be69b4b412a9f0dccf1c56336dfb17c1780ca7ae
elviabth <ejbenedith@gmail.com>
2024-05-22 17:33:50 +0200

    add custom styles

---
+++ spec/system/admin/admin_decidim_awesome_module_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe "Admin actions" do
+  let(:organization) { create(:organization) }
+  let!(:user) { create(:user, :confirmed, organization:) }
+  let!(:admin) { create(:user, :admin, :confirmed, organization:) }
+
+  before do
+    switch_to_host(organization.host)
+    login_as admin, scope: :user
+
+    visit decidim_admin.root_path
+  end
+
+  it "displays decidim-awesome-module in the admin menu" do
+    expect(page).to have_content("Decidim awesome")
+  end
+
+end
