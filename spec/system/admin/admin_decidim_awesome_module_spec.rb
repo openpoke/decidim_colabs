3f2d8a240e053ec8aa3ad599a9d87d778a35e2b3
elviabth <ejbenedith@gmail.com>
2024-05-23 09:53:35 +0200

    fix specs

--- spec/system/admin/admin_decidim_awesome_module_spec.rb
+++ spec/system/admin/admin_decidim_awesome_module_spec.rb
@@ -2,20 +2,21 @@
 
 require "rails_helper"
 
-describe "Admin actions" do
-  let(:organization) { create(:organization) }
-  let!(:user) { create(:user, :confirmed, organization:) }
-  let!(:admin) { create(:user, :admin, :confirmed, organization:) }
+describe "Admin" do
+  context "when admin access to dashboard" do
+    let(:organization) { create(:organization) }
+    let!(:user) { create(:user, :confirmed, organization:) }
+    let!(:admin) { create(:user, :admin, :confirmed, organization:) }
 
-  before do
-    switch_to_host(organization.host)
-    login_as admin, scope: :user
+    before do
+      switch_to_host(organization.host)
+      login_as admin, scope: :user
 
-    visit decidim_admin.root_path
+      visit decidim_admin.root_path
+    end
+
+    it "displays decidim-awesome-module in the admin menu" do
+      expect(page).to have_content("Decidim awesome")
+    end
   end
-
-  it "displays decidim-awesome-module in the admin menu" do
-    expect(page).to have_content("Decidim awesome")
-  end
-
 end
