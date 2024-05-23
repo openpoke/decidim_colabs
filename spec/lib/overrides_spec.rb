7b6398b26de0693fa73a2e40c6967feda37e3e82
Elvia Benedith <116598037+ElviaBth@users.noreply.github.com>
2024-05-23 16:33:49 +0200

    Custom colabs middle footer (#1)
    * install deface gem
    
    * custom Col·laboratori footer
    
    * upload wallop font
    
    * add middle_footer media queries
    
    * join wallop-medium fonts
    
    * add overrides_spec
    
    * fix rubocop
    
    ---------
    
    Co-authored-by: Ivan Vergés <ivan@pokecode.net>

---
+++ spec/lib/overrides_spec.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+# We make sure that the checksum of the file overriden is the same
+# as the expected. If this test fails, it means that the overriden
+# file should be updated to match any change/bug fix introduced in the core
+checksums = [
+  {
+    package: "decidim-core",
+    files: {
+      "/app/views/layouts/decidim/footer/_main.html.erb" => "86e880bb7751c32efbb53fadec653454"
+    }
+  }
+]
+
+describe "Overriden files", type: :view do
+  checksums.each do |item|
+    spec = Gem::Specification.find_by_name(item[:package])
+    item[:files].each do |file, signature|
+      it "#{spec.gem_dir}#{file} matches checksum" do
+        expect(md5("#{spec.gem_dir}#{file}")).to eq(signature)
+      end
+    end
+  end
+
+  private
+
+  def md5(file)
+    Digest::MD5.hexdigest(File.read(file))
+  end
+end
